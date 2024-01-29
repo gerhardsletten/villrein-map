cc7d0c424a2bf9ce68b47219cd0e29e00def47f1
Gerhard Sletten <gerhardsletten@gmail.com>
2024-01-29 23:18:53 +0100

    Fix position bottom ios

--- src/components/Slidepanel.tsx
+++ src/components/Slidepanel.tsx
@@ -5,20 +5,21 @@ export function SlidePanel({ children }: { children: ReactNode }) {
   const [expanded, setExpanded] = useState(false);
   return (
     <div
-      className={cx(
-        "absolute top-full w-screen h-[calc(50vh)] -mt- z-30 tra transition-all",
-        {
-          "-translate-y-full": expanded,
-        }
-      )}
+      className={cx("fixed top-full w-screen z-30 transition-all", {
+        "-translate-y-full h-[calc(50vh)]": expanded,
+      })}
     >
       <button
-        className="absolute bottom-full -mt-10 left-3 bg-black text-white p-2 px-6 rounded-t-lg text-sm"
+        className="absolute bottom-full left-3 bg-black text-white p-2 px-6 rounded-t-lg text-sm"
         onClick={() => setExpanded(!expanded)}
       >
         Tabell
       </button>
-      <div className="bg-black text-white p-4 h-full w-screen overflow-y-auto">
+      <div
+        className={cx(
+          "bg-black h-full text-white p-4 w-screen overflow-y-auto"
+        )}
+      >
         {children}
       </div>
     </div>
